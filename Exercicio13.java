f1cbef06305f8cb0269abf8db0740ce04149b11d
GuiKre <guilhermekre@gmail.com>
2024-03-24 18:13:55 -0300

    exercicio 13

---
+++ Exercicio13.java
@@ -0,0 +1,40 @@
+import ex13.Pessoa;
+
+public class Exercicio13 {
+
+    public static void executar(){
+
+        int totalAptos = 0;
+        int totalHomens = 0;
+        int totalMulheres = 0;
+
+        int numeroPessoas = Prompt.lerInteiro("Digite o número de pessoas: ");
+
+        for (int i = 1; i <= numeroPessoas; i++) {
+            Prompt.imprimir("Dados da pessoa " + i + ": ");
+            String nome = Prompt.lerLinha("Nome: ");
+
+            char sexo = Prompt.lerCaractere("Sexo (M/F): ");
+
+            int idade = Prompt.lerInteiro("Idade: ");
+
+            char saude = Prompt.lerCaractere("Saúde (S/N): ");
+
+            Pessoa pessoa = new Pessoa(nome, sexo, idade, saude);
+
+            if (pessoa.idade >= 18 && (pessoa.saude == 'S' || pessoa.saude == 's')) {
+                totalAptos++;
+            }
+
+            if (pessoa.sexo == 'M' || pessoa.sexo == 'm') {
+                totalHomens++;
+            } else {
+                totalMulheres++;
+            }
+        }
+
+        Prompt.imprimir("Total de pessoas aptas para o serviço militar: " + totalAptos);
+        Prompt.imprimir("Total de homens: " + totalHomens);
+        Prompt.imprimir("Total de mulheres: " + totalMulheres);
+    }
+}
---
+++ ex13/Pessoa.java
@@ -0,0 +1,16 @@
+package ex13;
+
+public class Pessoa {
+
+    public String nome;
+    public char sexo;
+    public int idade;
+    public char saude;
+
+    public Pessoa(String nome, char sexo, int idade, char saude) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.saude = saude;
+    }
+}
