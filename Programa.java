2a3c417556c6a172f8e950bf645b05d6b91df030
GuiKre <guilhermekre@gmail.com>
2024-03-26 16:47:52 -0300

    Menu de Execuções

--- Programa.java
+++ Programa.java
@@ -2,31 +2,36 @@ public class Programa {
 
     public static void main(String[] args){
 
-        //Exercicio01.executar();
-        //Exercicio02.executar();
-        //Exercicio03.executar();
-        //Exercicio04.executar();
-        //Exercicio05.executar();
-        //Exercicio06.executar();
-        //Exercicio07.executar();
-        //Exercicio08.executar();
-        //Exercicio09.executar();
-        //Exercicio10.executar();
-        //Exercicio11.executar();
-        //Exercicio12.executar();
-        //Exercicio13.executar();
-        //Exercicio14.executar();
-        //Exercicio15.executar();
-        //Exercicio16.executar();
-        //Exercicio17.executar();
-        //Exercicio18.executar();
-        //Exercicio19.executar();
-        //Exercicio20.executar();
-        //Exercicio21.executar();
-        //Exercicio22.executar();
-        //Exercicio23.executar();
-        //Exercicio24.executar();
-        //Exercicio25.executar();
-        Exercicio26.executar();
+        int num = Prompt.lerInteiro("Digite o número da questão que deseja executar(1 a 26): ");
+        
+        switch (num) {
+            case 1:  Exercicio01.executar(); break;
+            case 2:  Exercicio02.executar(); break;
+            case 3:  Exercicio03.executar(); break;
+            case 4:  Exercicio04.executar(); break;
+            case 5:  Exercicio05.executar(); break;
+            case 6:  Exercicio06.executar(); break;
+            case 7:  Exercicio07.executar(); break;
+            case 8:  Exercicio08.executar(); break;
+            case 9:  Exercicio09.executar(); break;
+            case 10: Exercicio10.executar(); break;
+            case 11: Exercicio11.executar(); break;
+            case 12: Exercicio12.executar(); break;
+            case 13: Exercicio13.executar(); break;
+            case 14: Exercicio14.executar(); break;
+            case 15: Exercicio15.executar(); break;
+            case 16: Exercicio16.executar(); break;
+            case 17: Exercicio17.executar(); break;
+            case 18: Exercicio18.executar(); break;
+            case 19: Exercicio19.executar(); break;
+            case 20: Exercicio20.executar(); break;
+            case 21: Exercicio21.executar(); break;
+            case 22: Exercicio22.executar(); break;
+            case 23: Exercicio23.executar(); break;
+            case 24: Exercicio24.executar(); break;
+            case 25: Exercicio25.executar(); break;
+            case 26: Exercicio26.executar(); break;
+            default: Prompt.imprimir("Número da questão inválido."); break;      
+        }
     }
 }
